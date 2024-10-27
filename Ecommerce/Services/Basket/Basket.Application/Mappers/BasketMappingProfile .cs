3cbb5dd07b6743a49d61dc35513f92f3db08d2fb
Safayet Hossain <emonsafayet@gmail.com>
2024-10-27 23:17:17 +0600

    basket service's implemented.

---
+++ Ecommerce/Services/Basket/Basket.Application/Mappers/BasketMappingProfile .cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Basket.Application.Mappers
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingCart, ShoppingCartResponse>().ReverseMap();
+            CreateMap<ShoppingCartItem, ShoppingCartItemResponse>().ReverseMap();
+        }
+    }
+}
